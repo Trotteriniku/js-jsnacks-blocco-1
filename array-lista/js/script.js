41b0eff9de7b2292cc6eae6be0c07c2ca8ada2bb
Trotteriniku <stefanozidda3@gmail.com>
2023-10-13 17:38:31 +0200

    cicle for ceck element

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,4 +1,21 @@
 const fridge =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
-const table =['pesca']
+const table =['pesca'];
+fridge.push( 'pesca');
+
 
 // mi serve un bottone per aprire il frigo e controllare se all'interno ci sta il cocomero
+const openFridge =document.querySelector('button');
+
+
+openFridge.addEventListener('click',function () {
+    for (let i = 0; i < fridge.length; i++) {
+        const element =  fridge[i];
+        console.log(element);
+
+        if (element === 'cocomero') {
+             console.log('ci sta menomale');
+        } else{
+             console.log('noo che peccato');
+        }
+    }
+})
