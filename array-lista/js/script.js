8964df5ed5f8d620079f62ef1b4ef3ca9c2e7e77
Trotteriniku <stefanozidda3@gmail.com>
2023-10-13 17:19:34 +0200

    array const dichiarate

---
+++ array-lista/js/script.js
@@ -0,0 +1,4 @@
+const fridge =['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola' ];
+const table =['pesca']
+
+// mi serve un bottone per aprire il frigo e controllare se all'interno ci sta il cocomero
