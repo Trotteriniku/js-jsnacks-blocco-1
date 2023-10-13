200bf4095fa9b764cd53e0bbaac80bfde19ccd11
Trotteriniku <stefanozidda3@gmail.com>
2023-10-13 18:33:43 +0200

    added alert on result

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -6,16 +6,28 @@ fridge.push( 'pesca');
 // mi serve un bottone per aprire il frigo e controllare se all'interno ci sta il cocomero
 const openFridge =document.querySelector('button');
 
-
+let flag= false;
 openFridge.addEventListener('click',function () {
+const alert=document.querySelector('.alert');
+alert.classList.remove('d-none')
+let msg;
     for (let i = 0; i < fridge.length; i++) {
         const element =  fridge[i];
-        console.log(element);
+        
 
         if (element === 'cocomero') {
-             console.log('ci sta menomale');
-        } else{
-             console.log('noo che peccato');
-        }
+            let flag= true;
+            if (flag === true) {
+              msg='Trovato! Devo solo preparare il cocktail.';
+              alert.classList.add('alert-success');
+              alert.innerText = msg;
+            } else {
+                msg = 'Oh no, devo uscire a comprare il cocomero!';
+                alert.classList.remove('alert-success');
+                alert.classList.remove('alert-warning');
+                alert.innerText = msg;  
+            } 
+        }  
     }
+    
 })
