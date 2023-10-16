b9572c82719b89e26503ab97de9583bdc146267e
Trotteriniku <stefanozidda3@gmail.com>
2023-10-16 10:56:19 +0200

    adjusted first snack

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -6,28 +6,34 @@ fridge.push( 'pesca');
 // mi serve un bottone per aprire il frigo e controllare se all'interno ci sta il cocomero
 const openFridge =document.querySelector('button');
 
-let flag= false;
+
+
 openFridge.addEventListener('click',function () {
 const alert=document.querySelector('.alert');
 alert.classList.remove('d-none')
 let msg;
+let flag= false;
+
     for (let i = 0; i < fridge.length; i++) {
         const element =  fridge[i];
-        
-
         if (element === 'cocomero') {
-            let flag= true;
+            flag= true;
+            
+        }
+     
+    }
+
+
             if (flag === true) {
-              msg='Trovato! Devo solo preparare il cocktail.';
-              alert.classList.add('alert-success');
-              alert.innerText = msg;
-            } else {
+                msg='Trovato! Devo solo preparare il cocktail.';
+                alert.classList.add('alert-success');
+                alert.innerText = msg; 
+            } else{
                 msg = 'Oh no, devo uscire a comprare il cocomero!';
                 alert.classList.remove('alert-success');
-                alert.classList.remove('alert-warning');
+                alert.classList.add('alert-warning');
                 alert.innerText = msg;  
-            } 
-        }  
-    }
-    
+            }
+
+                
 })
